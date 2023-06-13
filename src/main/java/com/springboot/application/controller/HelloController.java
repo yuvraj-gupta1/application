4e2db9dc6c194f878b06f690a63d92218f24eb3f
yuvraj-gupta1 <guptayuvraj2002@gmail.com>
2023-06-13 12:51:25 +0530

    first

---
+++ src/main/java/com/springboot/application/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.springboot.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String root() {
+		return "you did it ";
+	}
+	@RequestMapping("/application")
+	public String application() {
+		return "you run application succesfully !!!!!!";
+	}
+	
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello !!";
+	}
+	
+}
